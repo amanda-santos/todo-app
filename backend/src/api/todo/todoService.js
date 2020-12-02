8aa53fcbd3b3fec7980cc4f885e4f3dc65849a97
Amanda Santos <amandasf0200@gmail.com>
2020-12-02 15:37:52 -0300

    backend + frontend (in dev)

--- backend/src/api/todo/todoService.js
+++ backend/src/api/todo/todoService.js
@@ -3,7 +3,8 @@ const Todo = require('./todo')
 // métodos HTTP
 Todo.methods(['get', 'post', 'put', 'delete'])
 
-// para ativar as validações dos dados em métodos Update
+// new: true para retornar o novo objeto, atualizado, ao executar o Update
+// runValidators: true para ativar as validações dos dados em métodos Update
 Todo.updateOptions({new: true, runValidators: true})
 
 module.exports = Todo
